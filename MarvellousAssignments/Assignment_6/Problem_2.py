10567309489293410b4f2dfef62f693fbc336502
Rohit Chandrakant Gore <rohitchandrakantgore@gmail.com>
2021-05-25 14:35:49 +0530

    'assignment6'

---
+++ MarvellousAssignments/Assignment_6/Problem_2.py
@@ -0,0 +1,46 @@
+
+
+class Circle():
+    pi = 3.14
+    def __init__(self, radius = 0, area=0, circumference=0):
+        self.radius =radius
+        self.area = area
+        self.circumference = circumference
+        # print(self.radius, self.circumference, self.area)
+
+    def accept(self,radius):
+        self.radius = radius 
+
+
+    def calculateArea(self):
+        self.area = self.pi * (self.radius**2)
+
+    def calculateCircumference(self):
+        self.circumference = 2* self.pi * self.radius
+
+    def display(self):
+        print("Radius : ",self.radius)
+        print("Area : ",self.area)
+        print("Circumference : ",self.circumference)
+
+obj1 = Circle()
+obj1.accept(3)
+obj1.calculateArea()
+obj1.calculateCircumference()
+obj1.display()
+print(" ")
+obj2 = Circle()
+obj2.accept(5)
+obj2.calculateArea()
+obj2.calculateCircumference()
+obj2.display()
+
+
+# >python MarvellousAssignments/Assignment_6/Problem_2.py
+# Radius :  3
+# Area :  28.26
+# Circumference :  18.84
+
+# Radius :  5
+# Area :  78.5
+# Circumference :  31.400000000000002
---
+++ MarvellousAssignments/Assignment_6/Problem_3.py
@@ -0,0 +1,38 @@
+
+class Arithmetic():
+
+    def __init__(self, value1 = 0,value2 =0):
+        self.value1 = value1
+        self.value2 = value2
+
+    def accept(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+        return self.value1, self.value2
+
+    def addition(self):
+        return self.value2+ self.value1
+
+    def subtraction(self):
+        return self.value1 - self.value2
+
+    def division(self):
+        return self.value1 / self.value2
+
+    def multiplication(self):
+        return self.value1 * self.value2
+
+
+obj = Arithmetic()
+print("numbers enterd: ",obj.accept(10,20))
+print("Addtion: ", obj.addition())
+print("Subtraction: ", obj.subtraction())
+print("Multiplication: ", obj.multiplication())
+print("Division: ", obj.division())
+
+# >python MarvellousAssignments/Assignment_6/Problem_3.py
+# numbers enterd:  (10, 20)
+# Addtion:  30     
+# Subtraction:  -10
+# Multiplication:  200
+# Division:  0.5
